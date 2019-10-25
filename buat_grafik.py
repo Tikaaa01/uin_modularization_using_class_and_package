2f9ff13dbb1d49e13e68a39b82011e4a7da07a20
Tikaaa01 <rimielti@gmail.com>
2019-10-25 07:44:38 +0700

    Grafik

---
+++ buat_grafik.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+
+data = [15, 30, 45, 10] #100%
+
+fig1, ax1 = plt.subplots() #grafik
+ax1.pie(data, labels=labels, shadow=True)
+plt.show()
+
